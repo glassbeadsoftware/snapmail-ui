bfb30ba4a7741d3e64f30ab9de007ec3b986aff4
damien <doute.d@gmail.com>
2020-04-24 14:54:30 -0400

    minor refactor

--- dist/hc-bridge.js
+++ dist/hc-bridge.js
@@ -1,31 +1,28 @@
 var holochain_connection = holochainclient.connect();
 
+const call_dna = (functionName, params = {}) => {
+  return new Promise((succ, err)=>{
+    //connect(process.env.NODE_ENV==="development"?{ url: "ws://localhost:8888"}:undefined)
+    holochain_connection.then(async ({callZome, close}) => {
+      let response = await callZome('test-instance', 'snapmail', functionName)(params)
+      console.log('dna call: ' + functionName + '(' + JSON.stringify(params) + ')')
+      console.log(response)
+      //succ(response)
+      succ(JSON.parse(response))
+    }).catch(error=>{
+      err(error)
+    })
+  })
+}
+
 function getMyHandle(callback) {
-  holochain_connection.then(({callZome, close}) => {
-    callZome(
-      'test-instance',
-      'snapmail',
-      'get_my_handle',
-    )({}).then(result => callback(result));
-  });
+  call_dna('get_my_handle', {}).then(result => callback(result));
 }
 
 function getHandle(myAgentId, callback) {
-  holochain_connection.then(({callZome, close}) => {
-    callZome(
-      'test-instance',
-      'snapmail',
-      'get_handle',
-    )({agentId: myAgentId}).then(result => callback(result));
-  });
+  call_dna('get_handle', {agentId: myAgentId}).then(result => callback(result));
 }
 
 function setHandle(username, callback) {
-  holochain_connection.then(({callZome, close}) => {
-    callZome(
-      'test-instance',
-      'snapmail',
-      'set_handle',
-    )({name: username}).then(result => callback(result));
-  });
+  call_dna('set_handle', {name: username}).then(result => callback(result));
 }
--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,8 @@
  * @param callResult
  */
 function show_output(callResult) {
-  console.log(callResult);
   var span = document.getElementById('output');
-  var output = JSON.parse(callResult);
-  span.textContent = ' ' + output.Ok;
+  span.textContent = ' ' + callResult.Ok;
 }
 
 function setMyHandle() {
