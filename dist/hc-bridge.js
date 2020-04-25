e2c2eefb720e5de99808d5dec51a3e0ae186ebea
damien <doute.d@gmail.com>
2020-04-24 21:52:44 -0400

    complete hc-bridge.js

--- dist/hc-bridge.js
+++ dist/hc-bridge.js
@@ -3,7 +3,8 @@ var holochain_connection = holochainclient.connect();
 const call_dna = (functionName, params = {}) => {
   return new Promise((succ, err)=>{
     //connect(process.env.NODE_ENV==="development"?{ url: "ws://localhost:8888"}:undefined)
-    holochain_connection.then(async ({callZome, close}) => {
+    holochain_connection.then(async ({callZome, close, onSignal}) => {
+      // FIXME handle onSignal
       let response = await callZome('test-instance', 'snapmail', functionName)(params)
       console.log('dna call: ' + functionName + '(' + JSON.stringify(params) + ')')
       console.log(response)
@@ -15,6 +16,8 @@ const call_dna = (functionName, params = {}) => {
   })
 }
 
+// -- Handle -- //
+
 function getMyHandle(callback) {
   call_dna('get_my_handle', {}).then(result => callback(result));
 }
@@ -25,4 +28,38 @@ function getHandle(myAgentId, callback) {
 
 function setHandle(username, callback) {
   call_dna('set_handle', {name: username}).then(result => callback(result));
+}
+
+// -- Mail -- //
+
+function sendMail(mail, callback) {
+  call_dna('send_mail', {subject: mail.subject, payload: mail.payload, to: mail.to, cc: mail.cc, bcc: mail.bcc}).then(result => callback(result));
+}
+
+function getMail(otherAgentId, callback) {
+  call_dna('get_mail', {address: otherAgentId}).then(result => callback(result));
+}
+
+function getAllArrivedMail(callback) {
+  call_dna('get_all_arrived_mail', {}).then(result => callback(result));
+}
+
+function checkIncomingMail(callback) {
+  call_dna('check_incoming_mail', {}).then(result => callback(result));
+}
+
+function checkIncomingAck(callback) {
+  call_dna('check_incoming_ack', {}).then(result => callback(result));
+}
+
+function acknowledgeMail(address, callback) {
+  call_dna('acknowledge_mail', {inmail_address: address}).then(result => callback(result));
+}
+
+function acknowledgeMail(address, callback) {
+  call_dna('has_mail_been_received', {outmail_address: address}).then(result => callback(result));
+}
+
+function acknowledgeMail(address, callback) {
+  call_dna('has_ack_been_received', {inmail_address: address}).then(result => callback(result));
 }
