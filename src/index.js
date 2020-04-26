459b4eb96746e014b89397ef4cb5cd9a8364df7b
damien <doute.d@gmail.com>
2020-04-26 17:32:38 -0400

    structuring code wip

--- src/index.js
+++ src/index.js
@@ -8,70 +8,74 @@ import '@vaadin/vaadin-list-box';
 import '@vaadin/vaadin-split-layout';
 
 window.addEventListener('load', () => {
-  initUI();
+  initUi();
 });
 
 
-function initUI() {
+function initUi() {
+  initApp()
+  initMenuBar()
+  initFileBox()
+  initInMail()
+  initOutMail()
+  initActionBar()
+}
 
+function initApp() {
   // const firstNameField = document.querySelector('#firstName');
   // const addButton = document.querySelector('#addButton');
 
-  // -- Filebox Grid -- //
+  // -- App Bar -- //
+  getMyHandle(show_output)
+}
 
-  let fileboxItems = [
-    {
-      "username": "toto",
-      "subject": "hello mail",
-      "date": "42",
-    }]
+function initMenuBar() {
+  // Menu -- vaadin-menu-bar
+  const menu = document.querySelector('#MenuBar');
+  menu.addEventListener('item-selected', function(e)
+  {
+    document.querySelector('i').textContent = JSON.stringify(e.detail.value);
+  });
+  menu.items = [{
+    text: 'Project', children: [{
+      text: 'Users', children: [{ text: 'List' }, { text: 'Add' }]
+    }, {
+      text: 'Billing', children: [{ text: 'Invoices' }, { text: 'Balance Events' }]
+    },]
+  }, {
+    text: 'Account', children: [{ text: 'Edit Profile' }, { text: 'Privacy Settings' }]
+  }, { text: 'Sign Out' }];
+}
+
+function initOutMail() {
+
+}
+
+function initInMail() {
+  // ContactList -- vaadin-list-box
+}
+
+function initFileBox() {
+  // Filebox -- vaadin-grid
+  let fileboxItems = [{
+    "username": "toto", "subject": "hello mail", "date": "42",
+  }]
 
   document.querySelector('vaadin-grid').items = fileboxItems;
+}
 
-
-
-
-    // -- Menu -- //
-
-    const menu = document.querySelector('#MenuBar');
-    menu.addEventListener('item-selected', function(e) {
-      document.querySelector('i').textContent = JSON.stringify(e.detail.value);
-    });
-    menu.items = [
+function initActionBar() {
+  // Action -- vaadin-menu-bar
+  const actionMenu = document.querySelector('#ActionBar');
+  actionMenu.items = [
       {
-        text: 'Project',
-        children: [
-          {text: 'Users',
-            children: [{text: 'List'}, {text: 'Add'}]
-          },
-          {text: 'Billing',
-            children: [{text: 'Invoices'}, {text: 'Balance Events'}]
-          },
-        ]
+        text: 'Clear',
       },
       {
-        text: 'Account',
-        children: [
-          {text: 'Edit Profile'},
-          {text: 'Privacy Settings'}
-        ]
+        text: 'Snap', disabled: true,
       },
-      {text: 'Sign Out'}
+      {text: 'Send'}
     ];
-
-
-    // -- Action Bar --//
-
-    const actionMenu = document.querySelector('#ActionBar');
-    actionMenu.items = [
-        {
-          text: 'Clear',
-        },
-        {
-          text: 'Snap', disabled: true,
-        },
-        {text: 'Send'}
-      ];
   actionMenu.addEventListener('item-selected', function(e) {
       document.querySelector('i').textContent = JSON.stringify(e.detail.value);
     });
